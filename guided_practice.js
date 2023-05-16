f8496d36d1772333f9ab07a956f5ce85cbba0611
jcatekista <jcatekista@gmail.com>
2023-05-15 18:41:01 -0700

    added block 20 files and folders

---
+++ guided_practice.js
@@ -0,0 +1,15 @@
+//access root element
+const root = document.getElementById("root");
+//create h1 with Mona Lisa
+const h1 = document.createElement("h1");
+h1.innerHTML = "MONA LISA";
+//append h1 to root
+root.appendChild(h1);
+//set image to root
+const img = document.createElement("img");
+img.src = "./assets/mona_lisa.jpg";
+root.appendChild(img);
+//add paragraph desc
+const p = document.createElement("p");
+p.innerText = "The Mona Lisa is a famous painting.";
+root.appendChild(p);
